55ee064aba0202451d6ebbcf3262d59477caf9b8
GoldenPotato <goldenpotato137@gmail.com>
2023-06-18 08:58:40 +0800

    test

--- Src/Program.cs
+++ Src/Program.cs
@@ -2,5 +2,7 @@
 
 Console.WriteLine("Hello, World!");
 
+if (!Path.Exists("./db/"))
+    Directory.CreateDirectory("./db/");
 using var stringWriter = new StreamWriter("./db/tmp.txt");
 stringWriter.WriteLine("Hello, World!");
