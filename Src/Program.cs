a0cd5bcd07c902bfd1151678adeff7f1c51050e7
GoldenPotato <goldenpotato137@gmail.com>
2023-06-18 09:39:19 +0800

    add workflow

--- Src/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
-
-if (!Path.Exists("./db/"))
-    Directory.CreateDirectory("./db/");
-using var stringWriter = new StreamWriter("./db/tmp.txt");
-stringWriter.WriteLine("Hello, World!");
