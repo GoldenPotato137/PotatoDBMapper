8d1577cf23096b0aab2670f93d09aa479e2447fb
GoldenPotato <goldenpotato137@gmail.com>
2023-06-18 08:25:59 +0800

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+using var stringWriter = new StreamWriter("./db/tmp.txt");
+stringWriter.WriteLine("Hello, World!");
